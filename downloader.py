077a6b7bc823dd6d12dad7e31fd33774a1277b1a
Amal Rajan <amalrajan74@gmail.com>
2017-05-31 11:24:31 +0400

    Fixed bugs

--- downloader.py
+++ downloader.py
@@ -5,16 +5,8 @@ import os
 from tkinter import *
 import tkinter.filedialog as filedialog
 import tkinter.ttk as ttk
+import tkinter.messagebox as messagebox
 
-
-##source = urllib.request.urlopen("https://www.instagram.com/p/BTl6JxolTg3/?taken-by=windows&hl=en")
-##
-##soup = bs.BeautifulSoup(source, "html5lib")
-##
-##image_url = soup.find('meta', property="og:image")["content"]
-##
-##with open("image1.jpg", 'wb') as image:
-##    image.write(urllib.request.urlopen(image_url).read())
 
 class MainWindow(Tk):
     def __init__(self):
@@ -30,49 +22,63 @@ class MainWindow(Tk):
         self.l_title = Label(text="Instagram Image Downloader", font=("Segoe UI", 18))
         self.l_url = Label(text="URL:", font=("Segoe UI", 11))
         self.l_output_location = Label(text="Destination: ", font=("Segoe UI", 11))
-        self.e_url = ttk.Entry(self, textvariable=self.var0)
+        self.e_url = ttk.Entry(self, textvariable=self.var0, font=("Segoe UI", 10))
         self.b_attach_batch = ttk.Button(self, text="Batch", command=self.batch_download)
-        self.e_output_directory = ttk.Entry(text="Enter the output location", textvariable=self.var2, font=("Segoe UI", 11))
+        self.e_output_directory = ttk.Entry(text="Enter the output location", textvariable=self.var2,
+                                            font=("Segoe UI", 10))
         self.b_browse_directory = ttk.Button(self, text="Browse", command=self.browse_directory)
-        self.l_filename = Label(text="Filename: ", font=("Segoe UI", 11))
-        self.e_filename = ttk.Entry(self)
-        self.o_file_extension = ttk.OptionMenu(self, self.var1, "Select Extension", "                 .jpg",
-                                               "                 .png")
-        self.b_download = ttk.Button(self, text="Download Image(s)", command=self.download_images)
+        # self.l_filename = Label(text="Filename: ", font=("Segoe UI", 11))
+        # self.e_filename = ttk.Entry(self)
+        # self.o_file_extension = ttk.OptionMenu(self, self.var1, "Select Extension", "                 .jpg",
+        # "                 .png")
+        self.b_download = ttk.Button(self, text="Download Image(s)", command=self.check_entries)
 
         self.l_title.place(x=350, y=50, anchor=CENTER)
         self.l_url.place(x=60, y=120)
         self.e_url.place(x=100, y=120, width=450, height=30)
         self.b_attach_batch.place(x=560, y=120, width=120, height=30)
-        self.o_file_extension.place(x=560, y=220, height=30)
+        # self.o_file_extension.place(x=560, y=220, height=30)
         self.l_output_location.place(x=10, y=170)
         self.e_output_directory.place(x=100, y=170, height=30, width=450)
         self.b_browse_directory.place(x=560, y=170, height=30, width=120)
-        self.l_filename.place(x=25, y=220)
-        self.e_filename.place(x=100, y=220, height=30, width=450)
+        # self.l_filename.place(x=25, y=220)
+        # self.e_filename.place(x=100, y=220, height=30, width=450)
         self.b_download.place(x=100, y=270, height=35, width=120)
-
 
     def browse_directory(self):
         self.directory = filedialog.askdirectory()
         self.var2.set(self.directory)
 
+    def check_entries(self):
+        self.entries = ["self.e_url", "self.e_output_directory"]
+        for entry in self.entries:
+            if eval(entry+".get()") == "":
+                messagebox.showinfo("Insufficient data", message="Please check if you've filled all the details.")
+                break
+        else:
+            self.start_download()
+
+
     def batch_download(self):
         self.batchfile_location = filedialog.askopenfilename()
-        print(self.batchfile_location)
         if str(self.batchfile_location)[-3:] == 'txt':
             with open(self.batchfile_location) as self.batchfile:
                 self.data = self.batchfile.read().replace('\n', ';')
                 self.var0.set(self.data)
 
-    def download_images(self):
+    def start_download(self):
         self.download_list = self.e_url.get().split(';')
-        for self.url in self.download_list:
-            self.source = urllib.request.urlopen(self.url)
+        self.incrementor = 1
+
+        for url in self.download_list:
+            self.source = urllib.request.urlopen(url)
             self.soup = bs.BeautifulSoup(self.source, "html5lib")
             self.image = self.soup.find("meta", property="og:image")["content"]
-            with open(self.e_filename.get(), 'wb') as self.new_image:
+            self.image_title = "image{}.jpg".format(self.incrementor)
+            self.incrementor += 1
+            with open(self.image_title, 'wb') as self.new_image:
                 self.new_image.write(urllib.request.urlopen(self.image).read())
+
 
 app = MainWindow()
 app.mainloop()
