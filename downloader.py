91edccae78a74cbcc376e30a4c845c0fbcc39c68
Amal Rajan <amalrajan74@gmail.com>
2017-06-01 00:18:19 +0400

    ''

--- downloader.py
+++ downloader.py
@@ -1,6 +1,5 @@
 import bs4 as bs
 import urllib.request
-import sys
 import os
 from tkinter import *
 import tkinter.filedialog as filedialog
@@ -13,11 +12,13 @@ class MainWindow(Tk):
         Tk.__init__(self)
 
         self.resizable(False, False)
-        self.geometry("700x380+{0}+{1}".format(self.winfo_screenwidth()//2 - 350, self.winfo_screenheight()//2 - 190))
+        self.geometry("700x380+{0}+{1}".format(
+            self.winfo_screenwidth() // 2 - 350, self.winfo_screenheight() // 2 - 190))
 
         self.var0 = StringVar()
         self.var1 = StringVar()
         self.var2 = StringVar()
+        self.progress_bar_val = 0
 
         self.l_title = Label(text="Instagram Image Downloader", font=("Segoe UI", 18))
         self.l_url = Label(text="URL:", font=("Segoe UI", 11))
@@ -27,25 +28,20 @@ class MainWindow(Tk):
         self.e_output_directory = ttk.Entry(text="Enter the output location", textvariable=self.var2,
                                             font=("Segoe UI", 10))
         self.b_browse_directory = ttk.Button(self, text="Browse", command=self.browse_directory)
-        # self.l_filename = Label(text="Filename: ", font=("Segoe UI", 11))
-        # self.e_filename = ttk.Entry(self)
-        # self.o_file_extension = ttk.OptionMenu(self, self.var1, "Select Extension", "                 .jpg",
-        # "                 .png")
         self.b_download = ttk.Button(self, text="Download Image(s)", command=self.check_entries)
         self.b_advanced_options = ttk.Button(self, text="Advanced Options", command=self.advanced_options)
+        self.p_download_progress = ttk.Progressbar(self, orient="horizontal", length=200, mode="determinate")
 
         self.l_title.place(x=350, y=50, anchor=CENTER)
         self.l_url.place(x=60, y=120)
         self.e_url.place(x=100, y=120, width=450, height=30)
         self.b_attach_batch.place(x=560, y=120, width=120, height=30)
-        # self.o_file_extension.place(x=560, y=220, height=30)
         self.l_output_location.place(x=10, y=170)
         self.e_output_directory.place(x=100, y=170, height=30, width=450)
         self.b_browse_directory.place(x=560, y=170, height=30, width=120)
-        # self.l_filename.place(x=25, y=220)
-        # self.e_filename.place(x=100, y=220, height=30, width=450)
         self.b_download.place(x=560, y=270, height=35, width=120)
         self.b_advanced_options.place(x=560, y=220, width=120, height=30)
+        self.p_download_progress.place(x=100, y=270)
 
     def browse_directory(self):
         self.directory = filedialog.askdirectory()
@@ -57,14 +53,14 @@ class MainWindow(Tk):
     def check_entries(self):
         self.entries = ["self.e_url", "self.e_output_directory"]
         for entry in self.entries:
-            if eval(entry+".get()") == "":
+            if eval(entry + ".get()") == "":
                 messagebox.showinfo("Insufficient data", message="Please check if you've filled all the details.")
                 break
         else:
-            self. result = messagebox.askyesno("Start downloading?", message="Are you sure you want to start the download?")
+            self.result = messagebox.askyesno("Start downloading?",
+                                              message="Are you sure you want to start the download?")
             if self.result:
                 self.start_download()
-
 
     def batch_download(self):
         self.batchfile_location = filedialog.askopenfilename()
@@ -73,8 +69,17 @@ class MainWindow(Tk):
                 self.data = self.batchfile.read().replace('\n', ';')
                 self.var0.set(self.data)
 
+    def run_progress_bar(self):
+        self.progress_bar_val += 1
+        self.p_
+
+
     def start_download(self):
         self.download_list = self.e_url.get().split(';')
+        self.p_download_progress["value"] = self.progress_bar_val
+        self.p_download_progress["maximum"] = len(self.download_list)
+
+        self.run_progress_bar()
         self.incrementor = 1
 
         for url in self.download_list:
