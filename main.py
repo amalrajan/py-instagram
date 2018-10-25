60256a2445c0514acf472d14d410e4a13e54cfe5
Amal Rajan <amalrajan74@gmail.com>
2018-10-25 19:50:50 +0530

    Fix download issue on Linux systems

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
 import argparse
 import datetime
 import os
-import sys
 import urllib.request
 
 import bs4 as bs
 
 
 def argument_parser():
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	parser.add_argument('type', type=str, help="single / multiple")
-	parser.add_argument('url', type=str, help="url / path to text file")
-	parser.add_argument('-o', '--output', type=str,
-						default=os.getcwd(), help="output destination")
+    parser.add_argument('type', type=str, help="single / multiple")
+    parser.add_argument('url', type=str, help="url / path to text file")
+    parser.add_argument('-o', '--output', type=str,
+                        default=os.getcwd(), help="output destination")
 
-	args = parser.parse_args()
-	main(args)
-
-
-def download_image(url, dest):
-	source = urllib.request.urlopen(url).read()
-	soup = bs.BeautifulSoup(source, 'lxml')
-	image = soup.find('meta', property='og:iamge')['content']
-	title = str(datetime.datetime.now().replace(':', '-'))
-	path = "{}\{}.jpg".format(dest, title)
+    args = parser.parse_args()
+    main(args)
 
 
 def download_image(url, dest):
-	source = urllib.request.urlopen(url).read()
-	soup = bs.BeautifulSoup(source, 'lxml')
-	image = soup.find("meta", property="og:image")["content"]
-	title = str(datetime.datetime.now()).replace(':', '-')
-	path = "{}\{}.jpg".format(dest, title)
+    source = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
+    image = soup.find('meta', property='og:iamge')['content']
+    title = str(datetime.datetime.now().replace(':', '-'))
+    path = "{}/{}.jpg".format(dest, title)
 
-	with open(path, 'wb') as file:
-	file.write(urllib.request.urlopen(image).read())
+
+def download_image(url, dest):
+    source = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
+    image = soup.find("meta", property="og:image")["content"]
+    title = str(datetime.datetime.now()).replace(':', '-')
+    path = "{}/{}.jpg".format(dest, title)
+
+    with open(path, 'wb') as file:
+        file.write(urllib.request.urlopen(image).read())
 
 
 def main(args):
@@ -44,9 +43,9 @@ def main(args):
     elif args.type == "multiple":
         with open(args.url) as batch_file:
             urls = batch_file.read().split('\n')
-    for url in urls:
-        download_image(url, args.output)
+        for url in urls:
+            download_image(url, args.output)
 
 
 if __name__ == "__main__":
-	argument_parser()
+    argument_parser()
